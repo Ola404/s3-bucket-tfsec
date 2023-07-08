b2d738a6c29fcc968378d8aefe2431ac4cd2900a
Temmyrk <temmyransomekuti@gmail.com>
2023-07-08 13:06:53 -0600

    ci_tfsecfixes

--- main.tf
+++ main.tf
@@ -17,4 +17,21 @@ resource "aws_s3_bucket" "bucket" {
   versioning {
     enabled = true
   }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "temmy-tfsec-log-bucket"
+  acl    = "log-delivery-write"
 }
